db7fe8bc5e79210fbb0217259372bbd4da2483f1
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-11-24 15:46:29 +0000

    Tutorial 1 complete. Remember to check project.clj for indentation.

--- project.clj
+++ project.clj
@@ -3,5 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+
+  ;;Source code paths
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.597"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
+
+;;cljsbuild options config
+  :cljsbuild {:builds
+              [{;; CLJS source path
+                :source-paths ["src/cljs"]
+                ;;Closure compiler config
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+                           :optimizations :whitespace
+                           ;; pretty print generated JS code
+                           :pretty-print true}}]}
+
+;; clean JS files generated during build
+  :clean-targets ^{:protect false} [:target-path "resources/public/js"]
+
   :repl-options {:init-ns modern-cljs.core})
---
+++ src/cljs/modern_cljs/modern.cljs
@@ -0,0 +1,3 @@
+(ns modern-cljs.modern)
+
+(.write js/document "All your APIs are belong to us")
