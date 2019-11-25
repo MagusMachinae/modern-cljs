17cf3e34e4343aca65dcbd5a50506a1c73c58065
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-11-25 01:04:09 +0000

    Tutorial 7, compiler optimisation, complete.

--- project.clj
+++ project.clj
@@ -16,14 +16,37 @@
   :ring {:handler modern-cljs.core/handler}
 ;;cljsbuild options config
   :cljsbuild {:builds
-              [{;; CLJS source path
-                :source-paths ["src/cljs"]
+              {:dev
+               {;; CLJS source path
+                :source-paths ["src/brepl" "src/cljs"]
                 ;;Closure compiler config
                 :compiler {;; CLS generated JS script filename
-                           :output-to "resources/public/js/modern.js"
+                           :output-to "resources/public/js/modern_dbg.js"
                            :optimizations :whitespace
                            ;; pretty print generated JS code
-                           :pretty-print true}}]}
+                           :pretty-print true}}
+               :pre-prod
+               {;; CLJS source path
+                :source-paths ["src/brepl" "src/cljs"]
+                ;;Closure compiler config
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern_pre.js"
+                           :optimizations :simple
+                           ;; pretty print generated JS code
+                           :pretty-print false}}
+               :prod
+               {;; clojurescript source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure Compiler options
+                :compiler {;; the name of emitted JS script file
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; advanced optimization
+                           :optimizations :advanced
+
+                           ;; no need prettyfication
+                           :pretty-print false}}}}
 
 ;; clean JS files generated during build
   :clean-targets ^{:protect false} [:target-path "resources/public/js"]
